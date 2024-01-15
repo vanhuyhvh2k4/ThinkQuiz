209855eb79dcdfd61ffa2de69e5d52b592daa68f
vanhuyhvh2k4 <vanhuyhvh2004@gmail.com>
2024-01-15 12:41:36 +0700

    initial project

---
+++ ThinkQuiz.Api/Program.cs
@@ -0,0 +1,15 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+{
+    builder.Services.AddControllers();
+}
+
+
+var app = builder.Build();
+{
+    app.UseHttpsRedirection();
+
+    app.MapControllers();
+
+    app.Run();
+}
+
