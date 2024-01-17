922dbf7fefbae3f7bd72f811c2fc50892f083fcc
vanhuyhvh2k4 <vanhuyhvh2004@gmail.com>
2024-01-17 21:24:32 +0700

    config validator

---
+++ ThinkQuiz.Application/Common/Behaviors/ValidationBehavior.cs
@@ -0,0 +1,45 @@
+﻿using ErrorOr;
+using FluentValidation;
+using MediatR;
+
+namespace ThinkQuiz.Application.Common.Behaviors
+{
+	public class ValidationBehavior<TRequest, TResponse>
+		where TRequest : IRequest<TRequest>
+		where TResponse : IErrorOr
+	{
+		private readonly IValidator<TRequest>? _validator;
+
+        public ValidationBehavior(IValidator<TRequest>? validator = null)
+        {
+            _validator = validator;
+        }
+
+        public async Task<TResponse> Handle(
+            TRequest request,
+            RequestHandlerDelegate<TResponse> next,
+            CancellationToken cancellationToken)
+        {
+            // Before the handler
+            if (_validator is null)
+            {
+                return await next();
+            }
+            var validationResult = await _validator.ValidateAsync(request, cancellationToken);
+
+            // After the handler
+            if (validationResult.IsValid)
+            {
+                return await next();
+            }
+
+            var errors = validationResult.Errors
+                .ConvertAll(validationFailure => Error.Validation(
+                    validationFailure.PropertyName,
+                    validationFailure.ErrorMessage));
+
+            return (dynamic)errors;
+        }
+    }
+}
+
--- ThinkQuiz.Application/DependencyInjection.cs
+++ ThinkQuiz.Application/DependencyInjection.cs
@@ -1,15 +1,22 @@
-﻿using MediatR;
+﻿using FluentValidation;
+using System.Reflection;
+using MediatR;
 using Microsoft.Extensions.DependencyInjection;
+using ThinkQuiz.Application.Common.Behaviors;
 
 namespace ThinkQuiz.Application
 {
     public static class DependencyInjection
 	{
 		public static IServiceCollection AddApplication(this IServiceCollection services)
-		{
+        {
 			services.AddMediatR(typeof(DependencyInjection).Assembly);
 
-			return services;
+            services.AddScoped(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
+
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+
+            return services;
 		}
 	}
 }
