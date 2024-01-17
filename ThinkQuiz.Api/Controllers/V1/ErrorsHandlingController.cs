a2ca40b6513e7b193f5b87966de651d5039112b6
vanhuyhvh2k4 <vanhuyhvh2004@gmail.com>
2024-01-17 20:32:54 +0700

    error factory

---
+++ ThinkQuiz.Api/Controllers/V1/ErrorsHandlingController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ThinkQuiz.Api.Controllers.V1
+{
+    public class ErrorsHandlingController : Controller
+    {
+        [Route("/error")]
+        public IActionResult Error()
+        {
+            return Problem();
+        }
+    }
+}
+
--- ThinkQuiz.Api/Program.cs
+++ ThinkQuiz.Api/Program.cs
@@ -11,6 +11,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 var app = builder.Build();
 {
+    app.UseExceptionHandler("/error");
+
     app.UseHttpsRedirection();
 
     app.MapControllers();
