e41e0e9239468167e58b8b596bc0a6326b7319a8
vanhuyhvh2k4 <vanhuyhvh2004@gmail.com>
2024-01-17 20:27:17 +0700

    add user repository

---
+++ ThinkQuiz.Application/Common/Interfaces/Persistence/Repositories/IUserRepository.cs
@@ -0,0 +1,12 @@
+﻿using ThinkQuiz.Domain.UserAggregate;
+
+namespace ThinkQuiz.Application.Common.Interfaces.Persistence.Repositories
+{
+    public interface IUserRepository
+	{
+		User? GetUserByEmail(string email);
+
+		void Add(User user);
+	}
+}
+
---
+++ ThinkQuiz.Infrashstructure/Persistence/Repositories/UserRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using ThinkQuiz.Application.Common.Interfaces.Persistence.Repositories;
+using ThinkQuiz.Domain.UserAggregate;
+
+namespace ThinkQuiz.Infrashstructure.Persistence.Repositories
+{
+	public class UserRepository : IUserRepository
+	{
+        private readonly List<User> _users = new();
+
+        public void Add(User user)
+        {
+            _users.Add(user);
+        }
+
+        public User? GetUserByEmail(string email)
+        {
+            return _users.SingleOrDefault(u => u.Email == email);
+        }
+    }
+}
+
