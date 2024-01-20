36fb0d7d57d8f843909fbb996ad5dd5433f4959a
vanhuyhvh2k4 <vanhuyhvh2004@gmail.com>
2024-01-20 14:34:43 +0700

    student domain

---
+++ ThinkQuiz.Domain/StudentAggregate/Student.cs
@@ -0,0 +1,40 @@
+﻿using ThinkQuiz.Domain.Common.Models;
+using ThinkQuiz.Domain.StudentAggregate.ValueObjects;
+using ThinkQuiz.Domain.UserAggregate.ValueObjects;
+
+namespace ThinkQuiz.Domain.StudentAggregate
+{
+    public class Student : AggregateRoot<StudentId, Guid>
+    {
+        public UserId UserId { get; private set; }
+
+        public DateTime CreatedAt { get; private set; }
+
+        public DateTime UpdatedAt { get; private set; }
+
+        private Student(
+            StudentId id,
+            UserId userId,
+            DateTime createdAt,
+            DateTime updatedAt) : base(id)
+        {
+            UserId = userId;
+            CreatedAt = createdAt;
+            UpdatedAt = updatedAt;
+        }
+
+        public static Student Create(UserId userId)
+        {
+            return new(
+                StudentId.CreateUnique(),
+                userId,
+                DateTime.Now,
+                DateTime.Now);
+        }
+
+#pragma warning disable CS8618
+        private Student() { }
+#pragma warning restore CS8618
+    }
+}
+
