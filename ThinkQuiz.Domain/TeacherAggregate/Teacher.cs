ca85a38812c27c8fcc8a47ed899471c79d8b9317
vanhuyhvh2k4 <vanhuyhvh2004@gmail.com>
2024-01-20 14:24:37 +0700

    teacher domain

---
+++ ThinkQuiz.Domain/TeacherAggregate/Teacher.cs
@@ -0,0 +1,54 @@
+﻿using ThinkQuiz.Domain.Common.Models;
+using ThinkQuiz.Domain.TeacherAggregate.ValueObjects;
+using ThinkQuiz.Domain.UserAggregate.ValueObjects;
+
+namespace ThinkQuiz.Domain.TeacherAggregate
+{
+    public class Teacher : AggregateRoot<TeacherId, Guid>
+	{
+		public UserId UserId { get; private set; }
+
+		public string? Position { get; private set; }
+
+		public string? SchoolInforamtion { get; private set; }
+
+		public DateTime CreatedAt { get; private set; }
+
+		public DateTime UpdatedAt { get; private set; }
+
+        private Teacher(
+            TeacherId id,
+            UserId userId,
+            string? position,
+            string? schoolInformation,
+            DateTime createdAt,
+            DateTime updatedAt) : base(id)
+        {
+            UserId = userId;
+            Position = position;
+            SchoolInforamtion = schoolInformation;
+            CreatedAt = createdAt;
+            UpdatedAt = updatedAt;
+        }
+
+        public static Teacher Create(
+            UserId userId,
+            string? position,
+            string? schoolInformation
+            )
+        {
+            return new(
+                TeacherId.CreateUnique(),
+                userId,
+                position,
+                schoolInformation,
+                DateTime.Now,
+                DateTime.Now);
+        }
+
+#pragma warning disable CS8618
+        private Teacher() { }
+#pragma warning restore CS8618
+    }
+}
+
---
+++ ThinkQuiz.Domain/TeacherAggregate/ValueObjects/TeacherId.cs
@@ -0,0 +1,30 @@
+﻿using ThinkQuiz.Domain.Common.Models;
+
+namespace ThinkQuiz.Domain.TeacherAggregate.ValueObjects
+{
+    public class TeacherId : AggregateRootId<Guid>
+    {
+        public override Guid Value { get; protected set; }
+
+        private TeacherId(Guid value)
+        {
+            Value = value;
+        }
+
+        public static TeacherId CreateUnique()
+        {
+            return new(Guid.NewGuid());
+        }
+
+        public static TeacherId Create(Guid value)
+        {
+            return new(value);
+        }
+
+        public override IEnumerable<object> GetEqualityComponents()
+        {
+            yield return Value;
+        }
+    }
+}
+
