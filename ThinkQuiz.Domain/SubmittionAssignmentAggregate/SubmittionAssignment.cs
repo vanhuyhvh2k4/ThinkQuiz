204cb46c6c5a78efed0d1d7820e5e7c848de6656
vanhuyhvh2k4 <vanhuyhvh2004@gmail.com>
2024-01-20 16:34:07 +0700

    submittion assignment domain

---
+++ ThinkQuiz.Domain/SubmittionAssignmentAggregate/SubmittionAssignment.cs
@@ -0,0 +1,55 @@
+﻿using ThinkQuiz.Domain.AssignmentAggregate.ValueObjects;
+using ThinkQuiz.Domain.Common.Models;
+using ThinkQuiz.Domain.StudentAggregate.ValueObjects;
+using ThinkQuiz.Domain.SubmittionAssignmentAggregate.ValueObjects;
+
+namespace ThinkQuiz.Domain.SubmittionAssignmentAggregate
+{
+    public class SubmittionAssignment : AggregateRoot<SubmittionAssignmentId, Guid>
+	{
+        public StudentId StudentId { get; private set; }
+
+        public AssignmentId AssignmentId { get; private set; }
+
+        public string AnswerUrl { get; private set; }
+
+        public double? Point { get; private set; }
+
+        public string? Comment { get; private set; }
+
+        public bool IsShowPoint { get; private set; }
+
+        public bool IsSubmitAgain { get; private set; } = false;
+
+        public DateTime CreatedAt { get; private set; }
+
+        public DateTime? UpdatedAt { get; private set; }
+
+        private SubmittionAssignment(
+            SubmittionAssignmentId id,
+            StudentId studentId,
+            AssignmentId assignmentId,
+            string answerUrl,
+            DateTime createdAt) : base(id)
+        {
+            StudentId = studentId;
+            AssignmentId = assignmentId;
+            AnswerUrl = answerUrl;
+            CreatedAt = createdAt;
+        }
+
+        public static SubmittionAssignment Create(
+            StudentId studentId,
+            AssignmentId assignmentId,
+            string answerUrl)
+        {
+            return new(
+                SubmittionAssignmentId.CreateUnique(),
+                studentId,
+                assignmentId,
+                answerUrl,
+                DateTime.Now);
+        }
+    }
+}
+
---
+++ ThinkQuiz.Domain/SubmittionAssignmentAggregate/ValueObjects/SubmittionAssignmentId.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using ThinkQuiz.Domain.Common.Models;
+
+namespace ThinkQuiz.Domain.SubmittionAssignmentAggregate.ValueObjects
+{
+    public class SubmittionAssignmentId : AggregateRootId<Guid>
+    {
+        public override Guid Value { get; protected set; }
+
+        private SubmittionAssignmentId(Guid value)
+        {
+            Value = value;
+        }
+
+        public static SubmittionAssignmentId CreateUnique()
+        {
+            return new(Guid.NewGuid());
+        }
+
+        public static SubmittionAssignmentId Create(Guid value)
+        {
+            return new(value);
+        }
+
+        public override IEnumerable<object> GetEqualityComponents()
+        {
+            yield return Value;
+        }
+    }
+}
+
