f67b5f4f9183f8a2dcbbf003652db7232578c125
vanhuyhvh2k4 <vanhuyhvh2004@gmail.com>
2024-01-20 15:15:00 +0700

    assigment domain

---
+++ ThinkQuiz.Domain/AssignmentAggregate/Assignment.cs
@@ -0,0 +1,70 @@
+﻿using ThinkQuiz.Domain.AssignmentAggregate.ValueObjects;
+using ThinkQuiz.Domain.Common.Models;
+using ThinkQuiz.Domain.TeacherAggregate.ValueObjects;
+
+namespace ThinkQuiz.Domain.AssignmentAggregate
+{
+    public class Assignment : AggregateRoot<AssignmentId, Guid>
+	{
+        public TeacherId AuthorId { get; private set; }
+
+        public string Name { get; private set; }
+
+        public DateTime StartTime { get; private set; }
+
+        public DateTime EndTime { get; private set; }
+
+        public string? Content { get; private set; }
+
+        public string FileUrl { get; private set; }
+
+        public bool IsDeleted { get; private set; } = false;
+
+        public DateTime CreatedAt { get; private set; }
+
+        public DateTime? UpdatedAt { get; private set; }
+
+        private Assignment(
+            AssignmentId id,
+            TeacherId authorId,
+            string name,
+            DateTime startTime,
+            DateTime endTime,
+            string? content,
+            string fileUrl,
+            DateTime createdAt) : base(id)
+        {
+            AuthorId = authorId;
+            Name = name;
+            StartTime = startTime;
+            EndTime = endTime;
+            Content = content;
+            FileUrl = fileUrl;
+            CreatedAt = createdAt;
+        }
+
+        public static Assignment Create(
+            TeacherId authorId,
+            string name,
+            DateTime startTime,
+            DateTime endTime,
+            string? content,
+            string fileUrl)
+        {
+            return new(
+                AssignmentId.CreateUnique(),
+                authorId,
+                name,
+                startTime,
+                endTime,
+                content,
+                fileUrl,
+                DateTime.Now);
+        }
+
+#pragma warning disable CS8618
+        private Assignment() { }
+#pragma warning restore CS8618
+    }
+}
+
