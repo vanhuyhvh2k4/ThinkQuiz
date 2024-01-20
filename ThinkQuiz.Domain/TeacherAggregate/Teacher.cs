e25c1813fb060ddf62de5fb92ee092ea2e0f0870
vanhuyhvh2k4 <vanhuyhvh2004@gmail.com>
2024-01-20 19:31:59 +0700

    relationship teacher domain

--- ThinkQuiz.Domain/TeacherAggregate/Teacher.cs
+++ ThinkQuiz.Domain/TeacherAggregate/Teacher.cs
@@ -1,4 +1,7 @@
-﻿using ThinkQuiz.Domain.Common.Models;
+﻿using ThinkQuiz.Domain.AssignmentAggregate.ValueObjects;
+using ThinkQuiz.Domain.ClassAggregate.ValueObjects;
+using ThinkQuiz.Domain.Common.Models;
+using ThinkQuiz.Domain.ExamAggregate.ValueObjects;
 using ThinkQuiz.Domain.TeacherAggregate.ValueObjects;
 using ThinkQuiz.Domain.UserAggregate.ValueObjects;
 
@@ -6,13 +9,25 @@ namespace ThinkQuiz.Domain.TeacherAggregate
 {
     public class Teacher : AggregateRoot<TeacherId, Guid>
 	{
+        private readonly List<ClassId> _classIds = new();
+
+        private readonly List<AssignmentId> _assignmentIds = new();
+
+        private readonly List<ExamId> _examIds = new();
+
 		public UserId UserId { get; private set; }
 
 		public string Position { get; private set; }
 
 		public string SchoolInforamtion { get; private set; }
 
-		public DateTime CreatedAt { get; private set; }
+        public IReadOnlyList<ClassId> ClassIds => _classIds.AsReadOnly();
+
+        public IReadOnlyList<AssignmentId> AssignmentIds => _assignmentIds.AsReadOnly();
+
+        public IReadOnlyList<ExamId> ExamIds => _examIds.AsReadOnly();
+
+        public DateTime CreatedAt { get; private set; }
 
 		public DateTime? UpdatedAt { get; private set; }
 
