1984ed0cabaa3e3248acabce9c06a7eba24a6c1a
vanhuyhvh2k4 <vanhuyhvh2004@gmail.com>
2024-01-23 21:42:12 +0700

    student config

---
+++ ThinkQuiz.Infrashstructure/Persistence/Configurations/StudentConfigurations.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ThinkQuiz.Domain.StudentAggregate;
+using ThinkQuiz.Domain.UserAggregate;
+
+namespace ThinkQuiz.Infrashstructure.Persistence.Configurations
+{
+	public class StudentConfigurations : IEntityTypeConfiguration<Student>
+	{
+        public void Configure(EntityTypeBuilder<Student> builder)
+        {
+            builder.ToTable("Students");
+
+            builder.HasKey(student => student.Id);
+
+            builder.HasOne<User>()
+                .WithOne(user => user.Student)
+                .HasForeignKey<Student>(student => student.UserId);
+
+            builder.Property(student => student.CreatedAt);
+
+            builder.Property(student => student.UpdatedAt);
+        }
+    }
+}
+
