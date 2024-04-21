7122bf315c68711555a9c2c164c19d595a04488e
vanhuyhvh2k4 <vanhuyhvh2004@gmail.com>
2024-04-21 20:28:27 +0700

    commit

---
+++ ThinkQuiz.Api/Controllers/V1/ExamController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Xceed.Words.NET;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ThinkQuiz.Api.Controllers.V1
+{
+    public class ExamController : ApiController
+    {
+
+        [HttpPost("exams")]
+        public async Task<IActionResult> UploadExamDocx(IFormFile file)
+        {
+            if (file == null || file.Length == 0)
+            {
+                return BadRequest("No file uploaded.");
+            }
+
+            // Đọc nội dung của tệp docx
+            using (var stream = new MemoryStream())
+            {
+                await file.CopyToAsync(stream);
+                using (var document = DocX.Load(stream))
+                {
+                    string content = document.Text;
+
+                    return Ok(content);
+                }
+            }
+        }
+    }
+}
+
